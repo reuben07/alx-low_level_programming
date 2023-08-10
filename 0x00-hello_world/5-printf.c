2b0ee3070a1ccd8ab566745e54301f7741d369aa
reuben07 <wambugur7@gmail.com>
2023-08-10 13:45:41 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio>
+#include <stdio.h>
 /**
- *main - A program that prints using printf function
+ * main - A program that prints using printf function
  * Return: 0
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n);
-	retun (0);
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
+
