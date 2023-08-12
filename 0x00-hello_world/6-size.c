b135ffcd4a44e8545b184edee13c5de6f7bd07d5
reuben07 <wambugur7@gmail.com>
2023-08-12 12:01:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,4 @@ printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(l));
 return (0);
-}
+
