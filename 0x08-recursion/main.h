5da212cf939e6ebe8c3ed689296c0540050b38f2
reuben07 <wambugur7@gmail.com>
2023-08-31 12:45:36 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char);
 void _puts_recursion(char *s);
-int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
