1aaf68af76866cc65aecf22867032231cf35198c
reuben07 <wambugur7@gmail.com>
2023-09-11 11:49:07 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MARCOLIKE_H
+#define MARCOLIKE_H
+/**
+ * file: 3-funtion_like_marco
+ *
+ * @x: input value (an int)
+ *
+ * Desc: file that compute absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) = (x))
+
+#endif
